91ea21d749b8b9721f87250afe76d498e4f69ded
Gurshaan Singh <gurshaansinghtiwana@gmail.com>
2025-08-20 17:21:18 +0530

    Update Exp_3A.sql

--- Experiment 3/Exp_3A.sql
+++ Experiment 3/Exp_3A.sql
@@ -1,1 +1,21 @@
 -- Made by gurshaan
+
+use krg_1b;
+
+create table Employee (
+    emp_id int
+);
+
+insert into 
+    Employee
+values
+    (1),(2),(3),(4),(5),(6),(7),(8),(8),(9),(9);
+
+select 
+    max(emp_id)
+from (
+    select emp_id
+    from Employee
+    group by emp_id
+    having count(emp_id) = 1
+) as UniqueEmployees;
